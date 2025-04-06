50073ae7de5116b8f7a23a238df49909a8feca72
KaterinaDemianik <k.demianik12@gmail.com>
2025-04-06 20:55:53 +0300

    Add input.py and output.py with empty functions

---
+++ app/io/input.py
@@ -0,0 +1,9 @@
+import pandas as pd
+def read_from_console():
+    pass
+
+def read_from_file_builtin(filepath):
+    pass
+
+def read_from_file_pandas(filepath):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def write_to_console(text):
+    pass
+
+def write_to_file_builtin(filepath, text):
+    pass
+
+def write_to_file_pandas(filepath, data):
+    pass
